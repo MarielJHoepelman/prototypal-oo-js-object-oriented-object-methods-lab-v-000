9febe49c29a1bda55b86ea25409ab33bef4ed722
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-11-05 20:43:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  // this.veto = function() {
-  //   return "No, I must disagree"
-  // }
-  this.approve = function() {
-    return "You can do that!"
-  }
+  
   this.doCharity = function() {
     return "I like to help people."
   }
@@ -22,4 +17,8 @@ function BoardMember(name, homeState, training) {
 
 BoardMember.prototype.veto = function() {
   return "No, I must disagree"
+}
+
+BoardMember.prototype.approve = function() {
+  eturn "You can do that!"
 }
